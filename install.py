623ac1f8087a0203b13b183cb07f0b9a1b4ce23b
papuSpartan <mcgpapu@gmail.com>
2022-11-12 03:46:29 -0600

    initial commit

---
+++ install.py
@@ -0,0 +1,6 @@
+import launch
+
+if not launch.is_installed("certipie"):
+    launch.run_pip("install certipie", "requirements for auto-tls")
+if not launch.is_installed("certifi"):
+    launch.run_pip("install certifi", "requirements for auto-tls")
