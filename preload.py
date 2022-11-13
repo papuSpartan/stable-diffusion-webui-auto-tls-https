4504126db90c263c758eab286c5d07692eda1f5c
papuSpartan <mcgpapu@gmail.com>
2022-11-13 06:47:43 -0600

    improve readability and update self-sign argument definition

--- preload.py
+++ preload.py
@@ -1,2 +1,2 @@
 def preload(parser):
-	parser.add_argument("--self-sign", action='store_true', help="Trust a provided self-signed certificate passed using --tls-certfile or automatically generate and trust a key/cert pair to enable TLS", default=None)
+	parser.add_argument("--self-sign", action='store_true', help="Trust a provided key/certificate pair passed using --tls-certfile and --tls-keyfile", default=None)
