8d8cff26957970a2373260548176c30d2bee7af0
baselqt <86596446+baselqt@users.noreply.github.com>
2023-12-16 01:01:10 +0300

    specify version
    fix import error by specifying certipie version

--- install.py
+++ install.py
@@ -1,6 +1,6 @@
 import launch
 
 if not launch.is_installed("certipie"):
-    launch.run_pip("install certipie", "requirements for auto-tls")
+    launch.run_pip("install certipie==0.2.0", "requirements for auto-tls")
 if not launch.is_installed("certifi"):
     launch.run_pip("install certifi", "requirements for auto-tls")
